f0e01c03c93ac3de05818b913a0ec0d51b2e63ec
So... <yerdelenkus@gmail.com>
2026-09-05 00:21:04 +0400

    scss structure and theme

--- public/src/theme.js
+++ public/src/theme.js
@@ -1,8 +1,10 @@
 var theme = localStorage.getItem("theme") || "dark";
+const button = document.querySelector("[data-themebutton]");
 
 function themeReload(item){
     localStorage.setItem("theme" , item)
     document.documentElement.setAttribute("data-theme", item);
+    button.innerHTML = item === "dark" ? "light" : "dark"
 
 }
 function changeThem(){
